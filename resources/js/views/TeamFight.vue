9c53099257b65df43c41c535461c86e019578ec6
Daniel Fly Nygaard <danielflynygaard@gmail.com>
2021-12-09 22:13:37 +0100

    Fixed download logic

--- resources/js/views/TeamFight.vue
+++ resources/js/views/TeamFight.vue
@@ -314,7 +314,14 @@ export default {
                 },
                 fetchPolicy: "network-only"
             }).then(({data}) => {
-                window.location.href = data.export;
+                var file_path = data.export;
+                var a = document.createElement('A');
+                a.href = file_path;
+                a.download = file_path.substr(file_path.lastIndexOf('/') + 1);
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+
             }).catch((error) => {
                 this.$buefy.snackbar.open(
                     {
