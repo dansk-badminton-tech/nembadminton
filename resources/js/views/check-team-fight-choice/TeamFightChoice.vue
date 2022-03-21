313115757abdd99896aaacb6d810c0383b0de7a2
Daniel Fly Nygaard <dnyg@tdc.dk>
2022-03-21 23:18:13 +0100

    Updated choice

--- resources/js/views/check-team-fight-choice/TeamFightChoice.vue
+++ resources/js/views/check-team-fight-choice/TeamFightChoice.vue
@@ -1,19 +1,36 @@
 <template>
     <div class="columns">
-        <div class="column has-text-centered">
-            <h1 class="title">Gammel eller ny?</h1>
-            <h2 class="subtitle">Den nye har 2 begrænsninger (indtil videre): 1. Viser kun holdkampe mindre end 1 måned gammel. 2. viser kun hold fra danmarksserien og op</h2>
-            <div class="field has-addons is-justify-content-center">
-                <p class="control">
-                    <b-button tag="router-link" to="/team-fight/check" size="is-large">
-                        Gå til gammel
-                    </b-button>
-                </p>
-                <p class="control">
-                    <b-button tag="router-link" to="/team-fight-v2/check" size="is-large">
-                        Gå til ny
-                    </b-button>
-                </p>
+        <div class="column">
+            <h1 class="title">Ny version af "Tidligere holdkampe"</h1>
+            <div class="columns">
+                <div class="column">
+                    <div class="box">
+                        <div class="content">
+                            <p>Begrænsninger (indtil videre):</p>
+                            <ul>
+                                <li>Viser kun holdkampe mindre end 1 måned</li>
+                                <li>Viser kun hold fra danmarksserien og op</li>
+                                <li>Kun en rangliste dvs ingen specifik rangliste pr hold</li>
+                            </ul>
+                        </div>
+                        <b-button tag="router-link" to="/team-fight-v2/check" size="is-large">
+                            Gå til ny
+                        </b-button>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="box">
+                        <div class="content">
+                            <p>Begrænsninger:</p>
+                            <ul>
+                                <li>Ingen</li>
+                            </ul>
+                        </div>
+                        <b-button tag="router-link" to="/team-fight/check" size="is-large">
+                            Gå til gammel
+                        </b-button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
