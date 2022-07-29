d3cf7dfea7174a77f41befee010d8c3ac5b3a84c
Daniel Fly Nygaard <dfn@veo.co>
2022-07-29 12:52:24 +0200

    Forgot value

--- resources/js/views/check-team-fight/CheckTeamFight.vue
+++ resources/js/views/check-team-fight/CheckTeamFight.vue
@@ -13,7 +13,7 @@
                                 <option value="2019">2019/2020</option>
                                 <option value="2020">2020/2021</option>
                                 <option value="2021">2021/2022</option>
-                                <option value="2021">2022/2023</option>
+                                <option value="2022">2022/2023</option>
                             </b-select>
                         </b-field>
                     </b-step-item>
