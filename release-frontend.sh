62117ebd1e92fe37281d9687ce65f3e62b0b9a66
Daniel Fly Nygaard <dfn@veo.co>
2022-07-29 12:45:03 +0200

    Added images to deployment

--- release-frontend.sh
+++ release-frontend.sh
@@ -17,4 +17,6 @@ sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/w
 sshpass -p "${SSH_PASS}" scp -r public/css badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/css
 sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/js
 sshpass -p "${SSH_PASS}" scp -r public/js badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/js
+sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/images
+sshpass -p "${SSH_PASS}" scp -r public/images badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/images
 sshpass -p "${SSH_PASS}" scp public/mix-manifest.json badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/mix-manifest.json
