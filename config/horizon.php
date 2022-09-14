aad0d28af8f6ae526e05a00966a740e47f5e01cd
Daniel Fly Nygaard <dfn@veo.co>
2022-09-14 20:57:29 +0200

    Changed to 4 queue workers

--- config/horizon.php
+++ config/horizon.php
@@ -170,7 +170,7 @@ return [
                 'connection' => 'redis',
                 'queue'      => ['default'],
                 'balance'    => 'simple',
-                'processes'  => 2,
+                'processes'  => 4,
                 'tries'      => 2,
                 'nice'       => 0,
                 'memory' => 512,
