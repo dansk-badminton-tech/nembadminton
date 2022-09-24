b84d207dc0bdccbab608d03cd1f5f951a69bfcf5
Daniel Fly Nygaard <dfn@veo.co>
2022-09-24 10:36:26 +0200

    Updated horizon

--- database/migrations/2021_12_21_215514_create_view_members_with_points.php
+++ database/migrations/2021_12_21_215514_create_view_members_with_points.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\DB;
 
 class CreateViewMembersWithPoints extends Migration
 {
+
     /**
      * Run the migrations.
      *
@@ -25,14 +26,31 @@ class CreateViewMembersWithPoints extends Migration
         DB::statement($this->dropView());
     }
 
-    private function createView(): string
+    private function createView() : string
     {
         return <<<SQL
-CREATE OR REPLACE VIEW members_with_latest_points AS SELECT members.*, ( SELECT points.points FROM points WHERE members.id = points.member_id AND points.category IS NULL AND points.version =( SELECT MAX(VERSION) FROM points ) ) AS latest_level_points, ( SELECT points.position FROM points WHERE members.id = points.member_id AND points.category IS NULL AND points.version =( SELECT MAX(VERSION) FROM points ) ) AS latest_level_position, ( SELECT MAX(VERSION) FROM points ) AS latest_level_version FROM members;
+CREATE OR REPLACE view members_with_latest_points
+AS
+  SELECT members.*,
+         (SELECT points.points
+          FROM   points
+          WHERE  members.id = points.member_id
+                 AND points.category IS NULL
+                 AND points.version = (SELECT Max(version)
+                                       FROM   points)) AS latest_level_points,
+         (SELECT points.position
+          FROM   points
+          WHERE  members.id = points.member_id
+                 AND points.category IS NULL
+                 AND points.version = (SELECT Max(version)
+                                       FROM   points)) AS latest_level_position,
+         (SELECT Max(version)
+          FROM   points)                               AS latest_level_version
+  FROM   members;
 SQL;
     }
 
-    private function dropView(): string
+    private function dropView() : string
     {
         return <<<SQL
 DROP VIEW IF EXISTS `members_with_latest_points`;
