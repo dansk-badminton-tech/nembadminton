8fa0e4eae6c2ea581865b4da58063ec6f8c865f3
Daniel Fly Nygaard <dfn@veo.co>
2022-10-06 22:16:59 +0200

    Added indexes

---
+++ database/migrations/2022_10_06_220944_adding_indexes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('points', function (Blueprint $table) {
+            $table->index('category');
+            $table->index('points');
+            $table->index('version');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('points', function (Blueprint $table) {
+            $table->dropIndex('points_category_index');
+            $table->dropIndex('points_points_index');
+            $table->dropIndex('points_version_index');
+        });
+    }
+};
