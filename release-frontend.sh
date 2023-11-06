8616fb57e6253650ae314a0e699a91656f066cf9
Daniel Fly Nygaard <dfn@veo.co>
2023-11-06 20:59:37 +0100

    optimized frontend-deployment

--- release-frontend.sh
+++ release-frontend.sh
@@ -12,11 +12,14 @@ echo "Building frontend"
 yarn install --frozen-lockfile --no-progress
 yarn run production
 
-echo "Deploying frontend"
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/css
-sshpass -p "${SSH_PASS}" scp -r public/css badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/css
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/js
-sshpass -p "${SSH_PASS}" scp -r public/js badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/js
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/images
-sshpass -p "${SSH_PASS}" scp -r public/images badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/images
-sshpass -p "${SSH_PASS}" scp public/mix-manifest.json badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/mix-manifest.json
+echo "Removing old frontend"
+sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/css &
+sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/js &
+sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com rm -rf /var/www/badminton.social/projects/holdkamp/public/images &
+wait
+echo "Copying new frontend"
+sshpass -p "${SSH_PASS}" scp -r public/css badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/css &
+sshpass -p "${SSH_PASS}" scp -r public/js badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/js &
+sshpass -p "${SSH_PASS}" scp -r public/images badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/images &
+sshpass -p "${SSH_PASS}" scp public/mix-manifest.json badminton.social@linux368.unoeuro.com:/var/www/badminton.social/projects/holdkamp/public/mix-manifest.json &
+wait
