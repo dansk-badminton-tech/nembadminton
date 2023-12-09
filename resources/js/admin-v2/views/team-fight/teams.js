f7089483c1c822352cf518696d9aafe436a42471
Daniel Fly Nygaard <dfn@veo.co>
2023-12-09 20:07:34 +0100

    Added UX and started backend work

--- resources/js/admin-v2/views/team-fight/teams.js
+++ resources/js/admin-v2/views/team-fight/teams.js
@@ -1,4 +1,42 @@
 export class TeamFightHelper {
+    static generateSquad(mix, womenSingles, womenDoubles, mensSingles, mensDoubles) {
+        let categories = [];
+        let playerLimit = mix + womenSingles + womenDoubles + mensSingles + mensDoubles;
+
+        // Generate Mixed Doubles Categories
+        for (let i = 1; i <= mix; i++) {
+            categories.push({name: `${i}. MD`, category: "MD"});
+        }
+
+        // Generate Women Singles Categories
+        for (let i = 1; i <= womenSingles; i++) {
+            categories.push({name: `${i}. DS`, category: "DS"});
+        }
+
+        // Generate Women Doubles Categories
+        for (let i = 1; i <= womenDoubles; i++) {
+            categories.push({name: `${i}. DD`, category: "DD"});
+        }
+
+        // Generate Men Singles Categories
+        for (let i = 1; i <= mensSingles; i++) {
+            categories.push({name: `${i}. HS`, category: "HS"});
+        }
+
+        // Generate Men Doubles Categories
+        for (let i = 1; i <= mensDoubles; i++) {
+            categories.push({name: `${i}. HD`, category: "HD"});
+        }
+
+        return {
+            playerLimit: playerLimit,
+            league: 'OTHER',
+            categories: {
+                create: categories
+            }
+        };
+    }
+
     static generateSquadWith10Players() {
         return {
             playerLimit: 10,
--- tests/GraphQL/ValidateSquadTest.php
+++ tests/GraphQL/ValidateSquadTest.php
@@ -393,4 +393,75 @@ class ValidateSquadTest extends BaseTestCase
                 ]
             ]);
     }
+
+    /**
+     * @test
+     * @throws \JsonException
+     */
+    public function validatingSquadWithoutMDAndDSAndDD(): void
+    {
+        $women1 = CategoryFactory::makeWomen('MxD', 0, 'DD', 50, 0, '101010-1');
+        $women2 = CategoryFactory::makeWomen('MxD', 101, 'DD', 0, 0, '101010-2');
+        $women3 = CategoryFactory::makeWomen('DS', 50, 'DD', 50, 0, '101010-3');
+        $women4 = CategoryFactory::makeWomen('DS', 0, 'DD', 0, 0, '101010-4');
+
+        $men1 = CategoryFactory::makeMen('MxH', 0, 'HD', 250, 0, '101010-5');
+        $men2 = CategoryFactory::makeMen('MxH', 0, 'HD', 200, 0, '101010-6');
+        $men3 = CategoryFactory::makeMen('HS', 150, 'HD', 150, 0, '101010-7');
+        $men4 = CategoryFactory::makeMen('HS', 100, 'HD', 100, 0, '101010-8');
+        $men5 = CategoryFactory::makeMen('HS', 50, 'HD', 50, 0, '101010-9');
+        $men6 = CategoryFactory::makeMen('HS', 0, 'HD', 0, 0, '101010-10');
+
+        $squad = new Squad();
+        $squad->playerLimit = 10;
+        $squad->categories[] = CategoryFactory::makeCategory('HS. 1', 'HS', $men3);
+        $squad->categories[] = CategoryFactory::makeCategory('HS. 2', 'HS', $men4);
+        $squad->categories[] = CategoryFactory::makeCategory('HS. 3', 'HS', $men5);
+        $squad->categories[] = CategoryFactory::makeCategory('HS. 4', 'HS', $men6);
+        $squad->categories[] = CategoryFactory::makeCategory('HD. 1', 'HD', $men1, $men2);
+        $squad->categories[] = CategoryFactory::makeCategory('HD. 2', 'HD', $men3, $men4);
+        $squad->categories[] = CategoryFactory::makeCategory('HD. 3', 'HD', $men5, $men6);
+
+        $teamWrap = [
+            'name' => 'Team 1',
+            'squad' => $squad
+        ];
+
+        $teams = SerializerHelper::getSerializer()->encode([$teamWrap], 'json');
+
+        $variables = ['input' => json_decode($teams, true, 512, JSON_THROW_ON_ERROR)];
+        $this->graphQL(
+        /** @lang GraphQL */ '
+            mutation ($input: [ValidateTeam!]!) {
+              validateSquads(input: $input) {
+                refId
+                category
+                gender
+              }
+            }
+        ',
+            $variables
+        )
+             ->assertExactJson([
+                 'data' => [
+                     'validateSquads' => [
+                         [
+                             'refId' => $women1->refId,
+                             'category' => 'MD',
+                             'gender' => 'K'
+                         ],
+                         [
+                             'refId' => $men1->refId,
+                             'category' => 'MD',
+                             'gender' => 'M'
+                         ]
+                     ]
+                 ],
+                 'extensions' => [
+                     'lighthouse_subscriptions' => [
+                         'channel' => null
+                     ]
+                 ]
+             ]);
+    }
 }
