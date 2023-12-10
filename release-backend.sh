c079ceec21c4ebc13502b3a202227ae89bd146e2
Daniel Fly Nygaard <dfn@veo.co>
2023-12-10 12:06:37 +0100

    Removed sshpass

--- release-backend.sh
+++ release-backend.sh
@@ -4,22 +4,17 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-read -sp 'badminton.social@linux368.unoeuro.com SSH password: ' SSH_PASS
-echo ""
-
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com echo "Testing SSH password"
-
 echo "Deploying backend"
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com bash <<EOF
+ssh badminton.social@linux368.unoeuro.com bash <<EOF
 cd /var/www/badminton.social/projects/holdkamp
 git pull
 EOF
 
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com bash <<EOF
+ssh badminton.social@linux368.unoeuro.com bash <<EOF
 cd /var/www/badminton.social/projects/holdkamp
 php81 /usr/local/bin/composer install --ignore-platform-reqs
 EOF
 
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com php81 /var/www/badminton.social/projects/holdkamp/artisan migrate --force
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com php81 /var/www/badminton.social/projects/holdkamp/artisan lighthouse:clear-cache
-sshpass -p "${SSH_PASS}" ssh badminton.social@linux368.unoeuro.com php81 /var/www/badminton.social/projects/holdkamp/artisan lighthouse:cache
+ssh badminton.social@linux368.unoeuro.com php81 /var/www/badminton.social/projects/holdkamp/artisan migrate --force
+ssh badminton.social@linux368.unoeuro.com php81 /var/www/badminton.social/projects/holdkamp/artisan lighthouse:clear-cache
+badminton.social@linux368.unoeuro.com php81 /var/www/badminton.social/projects/holdkamp/artisan lighthouse:cache
