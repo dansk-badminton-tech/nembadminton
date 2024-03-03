4a83fd6b73b4c153b444141b4c6a4d6dece59b0e
Daniel Fly Nygaard <dfn@veo.co>
2024-03-03 12:32:48 +0100

    Removed PHP info and added special route for app.nembadminton.dk

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('notifications/{id}/dismiss', [NotificationController::class, 'dismiss']);
 Route::get('forgot-password-finish', 'Spa2Controller@index')->name('password.reset');
-Route::get('php-info', function () {
-    if (app()->environment('local')) {
-        phpinfo();
-    }
+
+Route::group(['domain' => 'app.nembadminton.dk'], function (){
+    Route::redirect('/', '/app/home');
+    Route::get('/{any}', 'Spa2Controller@index')->where('any', '.*');
 });
-Route::get('/app/v1/{any}', 'SpaController@index')->where('any', '.*');
 Route::get('/app/{any}', 'Spa2Controller@index')->where('any', '.*');
 Route::get('/{any}', 'MarketingSpaController@index')->where('any', '.*');
+
