fee7a6e7cad9c6bd24785f2aca5290dcac0df5b5
Daniel Fly Nygaard <dfn@veo.co>
2024-09-21 19:47:17 +0200

    Added 10 fight team matches

--- resources/js/admin-v2/views/team-fight/teams.js
+++ resources/js/admin-v2/views/team-fight/teams.js
@@ -169,4 +169,51 @@ export class TeamFightHelper {
         }
     }
 
+    static generateSquadSeries1() {
+        return {
+            playerLimit: 10,
+            league: 'OTHER',
+            categories: {
+                create: [
+                    {
+                        name: "1. HD",
+                        category: "HD",
+                    }, {
+                        name: "2. HD",
+                        category: "HD",
+                    },
+                    {
+                        name: "1. DD",
+                        category: "DD",
+                    },
+                    {
+                        name: "1. DS",
+                        category: "DS",
+                    },
+                    {
+                        name: "1. MD",
+                        category: "MD",
+                    }, {
+                        name: "1. HS",
+                        category: "HS",
+                    }, {
+                        name: "2. HS",
+                        category: "HS",
+                    }, {
+                        name: "3. HS",
+                        category: "HS",
+                    },
+                    {
+                        name: "3. HD",
+                        category: "HD",
+                    },
+                    {
+                        name: "2. DD",
+                        category: "DD",
+                    }
+                ]
+            }
+        }
+    }
+
 }
