cf6784503ff7f1f3d0fea159aec74ec18967ce39
Daniel Fly Nygaard <dfn@veo.co>
2025-07-18 23:29:47 +0200

    Upgraded packages

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,13 @@ export default defineConfig(
                 ignored: ['**/storage/**/*'],
             },
         },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    silenceDeprecations: ["color-functions", "global-builtin", "import"]
+                }
+            }
+        },
         plugins: [
             laravel([
                         'resources/js/admin-v2/main.js'
