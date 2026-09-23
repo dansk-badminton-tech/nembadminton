791e858df77c03089ab9b97b763ecf081158f3a3
Daniel Fly Nygaard <danielflynygaard@gmail.com>
2026-09-23 20:55:20 +0200

    Removed test

--- tests/Browser/TeamFightScenarioGuideTest.php
+++ tests/Browser/TeamFightScenarioGuideTest.php
@@ -15,20 +15,4 @@ class TeamFightScenarioGuideTest extends DuskTestCase
     use DatabaseTruncation;
 
     protected $seeder = 'TestingDataSeeder';
-
-    public function test_scenario_controls_link_to_the_scenario_guide(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $clubhouse = Clubhouse::first();
-            $teamRound = TeamRound::where('name', '3x13 Kamps - Valid')->first();
-
-            $browser->visit(new LoginPage())
-                ->loginSPA('testing@gmail.com', 'Test1234')
-                ->visit(new TeamFightEditPage($clubhouse->id, $teamRound->id))
-                ->assertSeeIn('@scenario-guide-link', 'Sådan arbejder du med scenarier')
-                ->click('@scenario-guide-link')
-                ->waitForLocation('/app/help/guides/arbejd-med-scenarier-i-en-holdrunde')
-                ->waitForText('Arbejd med scenarier i en holdrunde');
-        });
-    }
 }
